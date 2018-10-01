6725a3c6a9b8696ea55b4d8d14d0810cf381eadc
SeanBecker <techtronixwebdesign@gmail.com>
2018-09-30 22:50:33 -0500

    course assignment : componentize app

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// globally declared cmps
+import Header from './components/shared/Header.vue'
+import ServerList from './components/server/ServerList.vue'
+import ServerDetails from './components/server/ServerDetails.vue'
+import Footer from './components/shared/Footer.vue'
+
+Vue.component('app-header', Header)
+Vue.component('app-server-list', ServerList)
+Vue.component('app-server-details', ServerDetails)
+Vue.component('app-footer', Footer)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
