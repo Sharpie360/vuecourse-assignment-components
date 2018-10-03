ea0e84cdada412fae8128ac0e55f552617d57624
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-02 22:13:23 -0500

    assignment part2 : extending the server state management

---
+++ src/components/server/Server.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="list-group-item">
+    <h3>id: {{ id }} | status: {{ status }}</h3> 
+  </li>
+</template>
+
+<script>
+import ServerList from './ServerList.vue'
+  export default {
+    data: function() {
+      return {
+        id: 1,
+        status: 'normal'
+      }
+    },
+    props: []
+  }
+</script>
